bf8de354247175c490f816ae6a2836c11f6d83f3
PoojaShankarPrasad <pooja.shankarprasad@tarento.com>
2021-03-04 15:24:24 +0530

    minasidor complete

--- minasidor.py
+++ minasidor.py
@@ -28,46 +28,46 @@ driver.find_element_by_xpath('//*[@id="wrapper"]/section/div/mp-login-page/div/d
 # to enter the login button
 driver.find_element_by_xpath('//*[@id="wrapper"]/section/div/mp-login-page/div/div/mp-login/div/div/div/div[4]/div[2]/button').click()
 
-# # to open the Förbrukning(Consumption)
-# wait.until(presence_of_element_located((By.XPATH, '//*[@id="header"]/div/div/div/nav/ul/li[2]/a')))
-# driver.find_element_by_xpath('//*[@id="header"]/div/div/div/nav/ul/li[2]/a').click()
-# # to open the Sodra jarnvagsgatn 50 the district
-# wait.until(presence_of_element_located((By.XPATH, '//*[@id="wrapper"]/section/div/mp-consumption-corporate-volume-page/div[1]/div/div/div/div[1]/mp-subscription-menu/div/div[1]/div[1]')))
-# driver.find_element_by_xpath('//*[@id="wrapper"]/section/div/mp-consumption-corporate-volume-page/div[1]/div/div/div/div[1]/mp-subscription-menu/div/div[1]/div[1]').click()
-#
-# # to open the Ar(year)
-# wait.until(presence_of_element_located((By.XPATH,'//*[@id="wrapper"]/section/div/mp-consumption-corporate-volume-page/div[2]/div/mp-volume-graph/div/div[1]/div[2]')))
-# driver.find_element_by_xpath('//*[@id="wrapper"]/section/div/mp-consumption-corporate-volume-page/div[2]/div/mp-volume-graph/div/div[1]/div[2]').click()
-#
-#
-# # open the Dag(day)
-# wait.until(presence_of_element_located((By.XPATH, '//*[@id="wrapper"]/section/div/mp-consumption-corporate-volume-page/div[2]/div/mp-volume-graph/div/div[1]/div[2]')))
-# driver.find_element_by_xpath('//*[@id="wrapper"]/section/div/mp-consumption-corporate-volume-page/div[2]/div/mp-volume-graph/div/div[1]/div[2]').click()
-# for i in range(12):
-# #to open the monthly wise data
-#     wait.until(presence_of_element_located((By.XPATH, '//*[@id="wrapper"]/section/div/mp-consumption-corporate-volume-page/div[2]/div/mp-volume-graph/div/div[1]/div[6]/a')))
-#     driver.find_element_by_xpath('//*[@id="wrapper"]/section/div/mp-consumption-corporate-volume-page/div[2]/div/mp-volume-graph/div/div[1]/div[6]/a').click()
-# # to download the xls files
-#     wait.until(presence_of_element_located((By.XPATH, '//*[@id="wrapper"]/section/div/mp-consumption-corporate-volume-page/div[2]/div/mp-volume-graph/div/div[1]/div[5]/div/div')))
-#     driver.find_element_by_xpath('//*[@id="wrapper"]/section/div/mp-consumption-corporate-volume-page/div[2]/div/mp-volume-graph/div/div[1]/div[5]/div/div').click()
-#
-#
-# # to open the Sodra jarnvagsgatn 50 fastigheten
-# wait.until(presence_of_element_located((By.XPATH, '//*[@id="wrapper"]/section/div/mp-consumption-corporate-volume-page/div[1]/div/div/div/div[1]/mp-subscription-menu/div/div[1]/div[2]')))
-# driver.find_element_by_xpath('//*[@id="wrapper"]/section/div/mp-consumption-corporate-volume-page/div[1]/div/div/div/div[1]/mp-subscription-menu/div/div[1]/div[2]').click()
-#
-# # to open the Dag(day)
-# wait.until(presence_of_element_located((By.XPATH,'//*[@id="wrapper"]/section/div/mp-consumption-corporate-volume-page/div[2]/div/mp-volume-graph/div/div[1]/div[1]')))
-# driver.find_element_by_xpath('//*[@id="wrapper"]/section/div/mp-consumption-corporate-volume-page/div[2]/div/mp-volume-graph/div/div[1]/div[1]').click()
-#
-# for j in range(12):
-#     wait.until(presence_of_element_located((By.XPATH,'//*[@id="wrapper"]/section/div/mp-consumption-corporate-volume-page/div[2]/div/mp-volume-graph/div/div[1]/div[5]/a')))
-#     driver.find_element_by_xpath('//*[@id="wrapper"]/section/div/mp-consumption-corporate-volume-page/div[2]/div/mp-volume-graph/div/div[1]/div[5]/a').click()
-#
-#     # to download the xls files
-#     wait.until((presence_of_element_located((By.XPATH, '//*[@id="wrapper"]/section/div/mp-consumption-corporate-volume-page/div[2]/div/mp-volume-graph/div/div[1]/div[4]/div/div'))))
-#     driver.find_element_by_xpath('//*[@id="wrapper"]/section/div/mp-consumption-corporate-volume-page/div[2]/div/mp-volume-graph/div/div[1]/div[4]/div/div').click()
-#
+# to open the Förbrukning(Consumption)
+wait.until(presence_of_element_located((By.XPATH, '//*[@id="header"]/div/div/div/nav/ul/li[2]/a')))
+driver.find_element_by_xpath('//*[@id="header"]/div/div/div/nav/ul/li[2]/a').click()
+# to open the Sodra jarnvagsgatn 50 the district
+wait.until(presence_of_element_located((By.XPATH, '//*[@id="wrapper"]/section/div/mp-consumption-corporate-volume-page/div[1]/div/div/div/div[1]/mp-subscription-menu/div/div[1]/div[1]')))
+driver.find_element_by_xpath('//*[@id="wrapper"]/section/div/mp-consumption-corporate-volume-page/div[1]/div/div/div/div[1]/mp-subscription-menu/div/div[1]/div[1]').click()
+
+# to open the Ar(year)
+wait.until(presence_of_element_located((By.XPATH,'//*[@id="wrapper"]/section/div/mp-consumption-corporate-volume-page/div[2]/div/mp-volume-graph/div/div[1]/div[2]')))
+driver.find_element_by_xpath('//*[@id="wrapper"]/section/div/mp-consumption-corporate-volume-page/div[2]/div/mp-volume-graph/div/div[1]/div[2]').click()
+
+
+# open the Dag(day)
+wait.until(presence_of_element_located((By.XPATH, '//*[@id="wrapper"]/section/div/mp-consumption-corporate-volume-page/div[2]/div/mp-volume-graph/div/div[1]/div[2]')))
+driver.find_element_by_xpath('//*[@id="wrapper"]/section/div/mp-consumption-corporate-volume-page/div[2]/div/mp-volume-graph/div/div[1]/div[2]').click()
+for i in range(12):
+#to open the monthly wise data
+    wait.until(presence_of_element_located((By.XPATH, '//*[@id="wrapper"]/section/div/mp-consumption-corporate-volume-page/div[2]/div/mp-volume-graph/div/div[1]/div[6]/a')))
+    driver.find_element_by_xpath('//*[@id="wrapper"]/section/div/mp-consumption-corporate-volume-page/div[2]/div/mp-volume-graph/div/div[1]/div[6]/a').click()
+# to download the xls files
+    wait.until(presence_of_element_located((By.XPATH, '//*[@id="wrapper"]/section/div/mp-consumption-corporate-volume-page/div[2]/div/mp-volume-graph/div/div[1]/div[5]/div/div')))
+    driver.find_element_by_xpath('//*[@id="wrapper"]/section/div/mp-consumption-corporate-volume-page/div[2]/div/mp-volume-graph/div/div[1]/div[5]/div/div').click()
+
+
+# to open the Sodra jarnvagsgatn 50 fastigheten
+wait.until(presence_of_element_located((By.XPATH, '//*[@id="wrapper"]/section/div/mp-consumption-corporate-volume-page/div[1]/div/div/div/div[1]/mp-subscription-menu/div/div[1]/div[2]')))
+driver.find_element_by_xpath('//*[@id="wrapper"]/section/div/mp-consumption-corporate-volume-page/div[1]/div/div/div/div[1]/mp-subscription-menu/div/div[1]/div[2]').click()
+
+# to open the Dag(day)
+wait.until(presence_of_element_located((By.XPATH,'//*[@id="wrapper"]/section/div/mp-consumption-corporate-volume-page/div[2]/div/mp-volume-graph/div/div[1]/div[1]')))
+driver.find_element_by_xpath('//*[@id="wrapper"]/section/div/mp-consumption-corporate-volume-page/div[2]/div/mp-volume-graph/div/div[1]/div[1]').click()
+
+for j in range(12):
+    wait.until(presence_of_element_located((By.XPATH,'//*[@id="wrapper"]/section/div/mp-consumption-corporate-volume-page/div[2]/div/mp-volume-graph/div/div[1]/div[5]/a')))
+    driver.find_element_by_xpath('//*[@id="wrapper"]/section/div/mp-consumption-corporate-volume-page/div[2]/div/mp-volume-graph/div/div[1]/div[5]/a').click()
+
+    # to download the xls files
+    wait.until((presence_of_element_located((By.XPATH, '//*[@id="wrapper"]/section/div/mp-consumption-corporate-volume-page/div[2]/div/mp-volume-graph/div/div[1]/div[4]/div/div'))))
+    driver.find_element_by_xpath('//*[@id="wrapper"]/section/div/mp-consumption-corporate-volume-page/div[2]/div/mp-volume-graph/div/div[1]/div[4]/div/div').click()
+
 # to open the Rapporter(reports)
 wait.until(presence_of_element_located((By.XPATH, '//*[@id="header"]/div/div/div/nav/ul/li[4]/a')))
 driver.find_element_by_xpath('//*[@id="header"]/div/div/div/nav/ul/li[4]/a').click()
@@ -109,25 +109,57 @@ driver.find_element_by_xpath('/html/body/app-root/main/section/div/mp-reports-pa
 
 #
 time.sleep(1)
-#
-selectedYear = int(driver.find_element_by_css_selector('#mat-datepicker-2 > mat-calendar-header > div > div > button.mat-calendar-period-button.mat-button').click())
-driver.find_element_by_css_selector('#mat-datepicker-2 > div > mat-multi-year-view > table > tbody > tr:nth-child(2) > td:nth-child(1) > div').click()
-while selectedYear != fromYear:
-    if selectedYear > fromYear:
-        driver.find_element_by_xpath('/html/body/div[1]/div[2]/div/mat-datepicker-content/mat-calendar/mat-calendar-header/div/div/button[2]').click()
-    else:
-        driver.find_element_by_xpath('/html/body/div[1]/div[2]/div/mat-datepicker-content/mat-calendar/mat-calendar-header/div/div/button[3]').click()
-    selectedYear = int(driver.find_element_by_css_selector('#mat-datepicker-0 > mat-calendar-header > div > div > button.mat-calendar-period-button.mat-button').text)
-driver.find_element_by_css_selector('#mat-datepicker-2 > div > mat-year-view > table > tbody > tr:nth-child(2) > td:nth-child(1) > div').find_elements_by_tag_name('td')[fromMonth-1].click()
+# to select year
+selectedYear = driver.find_element_by_xpath('/html/body/div[1]/div[2]/div/mat-datepicker-content/mat-calendar/mat-calendar-header/div/div/button[1]').click()
+# to select 2020
+driver.find_element_by_xpath('/html/body/div[1]/div[2]/div/mat-datepicker-content/mat-calendar/div/mat-multi-year-view/table/tbody/tr[2]/td[1]/div').click()
+
+driver.find_element_by_xpath('/html/body/div[1]/div[2]/div/mat-datepicker-content/mat-calendar/div/mat-year-view/table/tbody/tr[2]/td[1]/div').click()
 driver.find_element_by_xpath('/html/body/div[1]/div[2]/div/mat-datepicker-content/mat-calendar/div/mat-month-view/table/tbody/tr[2]/td[2]/div').click()
+
+
+# to select year (2)
+wait.until(presence_of_element_located((By.XPATH,'/html/body/app-root/main/section/div/mp-reports-page/div[2]/div[1]/mp-reports/div/div/div[3]/div/div[2]/div[3]/mp-datepicker/input')))
+driver.find_element_by_xpath('/html/body/app-root/main/section/div/mp-reports-page/div[2]/div[1]/mp-reports/div/div/div[3]/div/div[2]/div[3]/mp-datepicker/input').click()
+
 time.sleep(1)
-selectedYear = int(driver.find_element_by_css_selector('body > section > div > div > div > div.b2bconsumptionblock > consumption > div:nth-child(1) > div > consumption-heat > div > div > consumption-heat-energy > div > div.col-xs-12.consumption-filter.flex-space-between > consumption-filter-time > div > div:nth-child(2) > consumption-filter-time-month > datepicker-range-selection > div > div:nth-child(2) > datepicker-dropdown > div > div > ul > li > div > div > div > table > thead > tr > th:nth-child(2) > button > strong').text)
-while selectedYear != toYear:
-    if selectedYear > toYear:
-        driver.find_element_by_xpath('/html/body/section/div/div/div/div[1]/consumption/div[1]/div/consumption-heat/div/div/consumption-heat-energy/div/div[1]/consumption-filter-time/div/div[2]/consumption-filter-time-month/datepicker-range-selection/div/div[2]/datepicker-dropdown/div/div/ul/li/div/div/div/table/thead/tr/th[1]/button').click()
-    else:
-        driver.find_element_by_xpath('/html/body/section/div/div/div/div[1]/consumption/div[1]/div/consumption-heat/div/div/consumption-heat-energy/div/div[1]/consumption-filter-time/div/div[2]/consumption-filter-time-month/datepicker-range-selection/div/div[2]/datepicker-dropdown/div/div/ul/li/div/div/div/table/thead/tr/th[3]/button').click()
-    selectedYear = int(driver.find_element_by_css_selector('body > section > div > div > div > div.b2bconsumptionblock > consumption > div:nth-child(1) > div > consumption-heat > div > div > consumption-heat-energy > div > div.col-xs-12.consumption-filter.flex-space-between > consumption-filter-time > div > div:nth-child(2) > consumption-filter-time-month > datepicker-range-selection > div > div:nth-child(2) > datepicker-dropdown > div > div > ul > li > div > div > div > table > thead > tr > th:nth-child(2) > button > strong').text)
-driver.find_element_by_css_selector('body > section > div > div > div > div.b2bconsumptionblock > consumption > div:nth-child(1) > div > consumption-heat > div > div > consumption-heat-energy > div > div.col-xs-12.consumption-filter.flex-space-between > consumption-filter-time > div > div:nth-child(2) > consumption-filter-time-month > datepicker-range-selection > div > div:nth-child(2) > datepicker-dropdown > div > div > ul > li > div > div > div > table > tbody').find_elements_by_tag_name('td')[toMonth-1].click()
+
+selectedYear = driver.find_element_by_xpath('/html/body/div[1]/div[2]/div/mat-datepicker-content/mat-calendar/div/mat-month-view/table/tbody/tr[2]/td[1]/div').click()
+
+# to select csv files
+wait.until(presence_of_element_located((By.XPATH,'/html/body/app-root/main/section/div/mp-reports-page/div[2]/div[1]/mp-reports/div/div/div[3]/div/div[2]/div[4]/select')))
+driver.find_element_by_xpath('/html/body/app-root/main/section/div/mp-reports-page/div[2]/div[1]/mp-reports/div/div/div[3]/div/div[2]/div[4]/select').click()
+
+# to clicl on csv
+wait.until(presence_of_element_located((By.XPATH,'/html/body/app-root/main/section/div/mp-reports-page/div[2]/div[1]/mp-reports/div/div/div[3]/div/div[2]/div[4]/select/option[3]')))
+driver.find_element_by_xpath('/html/body/app-root/main/section/div/mp-reports-page/div[2]/div[1]/mp-reports/div/div/div[3]/div/div[2]/div[4]/select/option[3]').click()
+
+# to download the report
+wait.until(presence_of_element_located((By.XPATH,'/html/body/app-root/main/section/div/mp-reports-page/div[2]/div[1]/mp-reports/div/div/div[3]/div/div[2]/div[5]')))
+driver.find_element_by_xpath('/html/body/app-root/main/section/div/mp-reports-page/div[2]/div[1]/mp-reports/div/div/div[3]/div/div[2]/div[5]').click()
+# while selectedYear != fromYear:
+#     if selectedYear >> fromYear:
+#         driver.find_element_by_xpath('/html/body/div[1]/div[2]/div/mat-datepicker-content/mat-calendar/mat-calendar-header/div/div/button[2]').click()
+#     else:
+#         driver.find_element_by_xpath('/html/body/div[1]/div[2]/div/mat-datepicker-content/mat-calendar/mat-calendar-header/div/div/button[3]').click()
+#     selectedYear = driver.find_element_by_css_selector('/html/body/div[1]/div[2]/div/mat-datepicker-content/mat-calendar/mat-calendar-header/div/div/button[1]').click()
+# #  to select Month
+# driver.find_element_by_css_selector('#mat-datepicker-2 > div > mat-year-view > table > tbody > tr:nth-child(2) > td:nth-child(1) > div').find_elements_by_tag_name('td')[fromMonth-1].click()
+# # to select day
+# driver.find_element_by_xpath('//*[@id="mat-datepicker-2"]/div/mat-month-view/table/tbody/tr[2]/td[2]/div]').click()
+#
+# wait.until(presence_of_element_located((By.XPATH,'/html/body/app-root/main/section/div/mp-reports-page/div[2]/div[1]/mp-reports/div/div/div[3]/div/div[2]/div[2]/mp-datepicker/input')))
+# driver.find_element_by_xpath('/html/body/app-root/main/section/div/mp-reports-page/div[2]/div[1]/mp-reports/div/div/div[3]/div/div[2]/div[2]/mp-datepicker/input').click()
+#
+# time.sleep(1)
+#
+# selectedYear = int(driver.find_element_by_css_selector('body > section > div > div > div > div.b2bconsumptionblock > consumption > div:nth-child(1) > div > consumption-heat > div > div > consumption-heat-energy > div > div.col-xs-12.consumption-filter.flex-space-between > consumption-filter-time > div > div:nth-child(2) > consumption-filter-time-month > datepicker-range-selection > div > div:nth-child(2) > datepicker-dropdown > div > div > ul > li > div > div > div > table > thead > tr > th:nth-child(2) > button > strong').text)
+# while selectedYear != toYear:
+#     if selectedYear > toYear:
+#         driver.find_element_by_xpath('/html/body/section/div/div/div/div[1]/consumption/div[1]/div/consumption-heat/div/div/consumption-heat-energy/div/div[1]/consumption-filter-time/div/div[2]/consumption-filter-time-month/datepicker-range-selection/div/div[2]/datepicker-dropdown/div/div/ul/li/div/div/div/table/thead/tr/th[1]/button').click()
+#     else:
+#         driver.find_element_by_xpath('/html/body/section/div/div/div/div[1]/consumption/div[1]/div/consumption-heat/div/div/consumption-heat-energy/div/div[1]/consumption-filter-time/div/div[2]/consumption-filter-time-month/datepicker-range-selection/div/div[2]/datepicker-dropdown/div/div/ul/li/div/div/div/table/thead/tr/th[3]/button').click()
+# selectedYear = int(driver.find_element_by_css_selector('body > section > div > div > div > div.b2bconsumptionblock > consumption > div:nth-child(1) > div > consumption-heat > div > div > consumption-heat-energy > div > div.col-xs-12.consumption-filter.flex-space-between > consumption-filter-time > div > div:nth-child(2) > consumption-filter-time-month > datepicker-range-selection > div > div:nth-child(2) > datepicker-dropdown > div > div > ul > li > div > div > div > table > thead > tr > th:nth-child(2) > button > strong').text)
+# driver.find_element_by_css_selector('body > section > div > div > div > div.b2bconsumptionblock > consumption > div:nth-child(1) > div > consumption-heat > div > div > consumption-heat-energy > div > div.col-xs-12.consumption-filter.flex-space-between > consumption-filter-time > div > div:nth-child(2) > consumption-filter-time-month > datepicker-range-selection > div > div:nth-child(2) > datepicker-dropdown > div > div > ul > li > div > div > div > table > tbody').find_elements_by_tag_name('td')[toMonth-1].click()
 
 
